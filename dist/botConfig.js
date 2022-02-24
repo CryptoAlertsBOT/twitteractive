ffeeca7ac92bb16d2c1cb0b137d18ed66ec695e9
Kautilya Kondragunta <kautilyakondragunta@gmail.com>
2022-02-23 20:17:07 -0500

    project init
    - init repository & base files.

---
+++ dist/botConfig.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const process_1 = require("process");
+require("dotenv/config");
+console.log(process_1.env);
+//# sourceMappingURL=botConfig.js.map
