f99dfc33407e008a558efead5749b107c4afa3c8
Ankit Vaity <30876031+AnkitVaity@users.noreply.github.com>
2022-04-13 16:44:02 -0400

    Adding Plurged ALert

---
+++ src/models/PlurgedAlert/index.ts
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+import {Schema, Types} from "mongoose";
+import { IPurgedAlertModel, IPurgedAlertSchema } from "./types";
+
+const PurgedAlertSchema: Schema<IPurgedAlertSchema> = new Schema({
+    symbol: {
+        type: String,
+        required: true
+    },
+    userID: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    trigger_price: {
+        type: Number,
+        required: true,
+        min: [0, "Trigger price cannot be negative"],
+    },
+    price_when_set: {
+        type: Number,
+        required: true,
+        min: [0, "Current price cannot be negative or 0"]
+    },
+    deletedOn: {
+        type: Date,
+        requried: true,
+    }
+    
+}, {timestamps: {createdAt: true, updatedAt: false, currentTime: () => new Date()}});
+
+
+/**
+ * Middleware to run post deletion
+ */
+PurgedAlertSchema.pre('validate', function (next) {
+
+    // set `deletedOn`
+    this.set('deletedOn', new Date());
+    next();
+});
+
+
+export const PurgedAlert: IPurgedAlertModel = mongoose.model<IPurgedAlertSchema>("PurgedAlert", PurgedAlertSchema);
